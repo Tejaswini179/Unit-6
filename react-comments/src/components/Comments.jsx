016cc6fba68cfb0c20f13d145fdfee827fba015a
Tejaswini Khengare <97454567+Tejaswini179@users.noreply.github.com>
2022-05-29 19:59:57 +0530

    react comments

--- react-comments/src/components/Comments.jsx
+++ react-comments/src/components/Comments.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Comments.css"
+const Comment = ({ data }) => {
+    const [spread, setspread] = useState(false);
 
-export default function Comments({aid}) {
-  return (
-    <div>
-      <h1>{aid}</h1>
-    </div>
-  )
-}
+    if (data.replies) {
+        return (
+            <div>
+                <div className="MainCommentDiv">
+                    <div className="btnDiv">
+                        <button
+                            className="btn"
+                            onClick={() => setspread(!spread)}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className="upperDiv">
+                        <div>
+                            <div className="timeDiv">
+                                <p>{data.author}</p>
+                                <p>{data.points} points</p>
+                                <p>
+                                    {Math.floor(Math.random() * 59)} minutes ago
+                                </p>
+                            </div>
+                        </div>
+                        <div className="comment">
+                            <p>{data.body}</p>
+                        </div>
+                        <div className="replyDiv">
+                            <p>Reply</p>
+                            <p>Give Award</p>
+                            <p>Share</p>
+                            <p>Report</p>
+                            <p>Save</p>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    style={{
+                        display: spread ? "block" : "none",
+                        paddingLeft: "90px",
+                        borderLeft: "3.5px solid black",
+                        marginLeft: "8px",
+                    }}
+                >
+                    {data.replies.map((e) => {
+                        return <Comment key={e.id} data={e} />;
+                    })}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="MainCommentDiv endMainCommentDiv">
+                <div className="btnDiv hideBtn">
+                    <button className="btn" onClick={() => setspread(!spread)}>
+                        +
+                    </button>
+                </div>
+                <div className="upperDiv">
+                    <div>
+                        <div className="timeDiv">
+                            <p>{data.author}</p>
+                            <p>{data.points} points</p>
+                            <p>{Math.floor(Math.random() * 59)} minutes ago</p>
+                        </div>
+                    </div>
+                    <div className="comment">
+                        <p>{data.body}</p>
+                    </div>
+                    <div className="replyDiv">
+                        <p>Reply</p>
+                        <p>Give Award</p>
+                        <p>Share</p>
+                        <p>Report</p>
+                        <p>Save</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default Comment;
